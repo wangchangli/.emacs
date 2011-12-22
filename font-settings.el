89fdedfbe68c5b9117999f8a39c57137bf797e95
wangchangli <changliwang2009@gmail.com>
2011-12-23 01:58:21 +0800

    font

--- font-settings.el
+++ font-settings.el
@@ -2,14 +2,7 @@
 
 ;; Time-stamp: <2011-10-30 20:44:16 Sunday by wangchangli>
 
-(when (and window-system is-after-emacs-23)
- ;(require 'my-fontset-win)
-  (if mswin
-      ;; (huangq-fontset-courier 17)
-      (huangq-fontset-consolas 17)
-    ;; (huangq-fontset-dejavu 17)))
-    ;; (huangq-fontset-fixedsys 17)
-    (set-default-font "lucida console-10")
-))
+(set-default-font "lucida console-10")
+
 
 (provide 'font-settings)
