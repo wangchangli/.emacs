90b4ae8d35743abdbcb85c878ccd3effadd173f8
wangchangli <icharlie@wangchangli.(none)>
2012-05-01 17:42:28 +0800

    dc

--- slime-settings.el
+++ slime-settings.el
@@ -2,4 +2,15 @@
 (require 'slime)
 (slime-setup '(slime-repl))
 
+
+(require 'paredit)
+(define-key slime-mode-map (kbd "(") 'paredit-open-parenthesis)
+(define-key slime-mode-map (kbd ")") 'paredit-close-parenthesis)
+(define-key slime-mode-map (kbd "\"") 'paredit-doublequote)
+(define-key slime-mode-map (kbd "\\") 'paredit-backslash)
+
+(define-key slime-mode-map (kbd "RET") 'paredit-newline)
+(define-key slime-mode-map (kbd "<return>") 'paredit-newline)
+(define-key slime-mode-map (kbd "C-j") 'newline)
+
 (provide 'slime-settings)
